1d87b5c2ccfd2a0b8aac096991ce0c658266f042
TheLukCraft <lukaszcapala97@gmail.com>
2022-02-10 10:48:30 +0100

    3.3 Create a resource with the get method (after AutoMapper)

---
+++ WebAPI/Controllers/PostsController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : Controller
+    {
+        private readonly IPostService _postService;
+        public PostsController(IPostService postService)
+        {
+            _postService = postService;
+        }
+        [SwaggerOperation(Summary = "Retrieves all posts")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var posts = _postService.GetAllPosts();
+            return Ok(posts);
+        }
+        [SwaggerOperation(Summary = "Retrieves a specific post by uniqye id")]
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var post = _postService.GetPostById(id);
+            if (post == null)
+                return NotFound();
+            return Ok(post);
+        }
+    }
+}
